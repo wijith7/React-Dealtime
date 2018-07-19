565fbe07e0f753228d5382a478a71e582c635dd3
Wijith Bandara <wijith7@gmail.com>
2018-07-19 17:14:02 +0530

    buttons

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -6,7 +6,7 @@
 * in compliance with the License.
 * You may obtain a copy of the License at
 *
-*   http://www.apache.org/licenses/LICENSE-2.0
+* http://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing,
 * software distributed under the License is distributed on an
@@ -17,26 +17,22 @@
 */
 
 //Dependencies
-import React from 'react';
-import find from 'lodash/find';
-import { Icon } from 'react-materialize';
-import { Link } from 'react-router-dom';
-import { getProducts } from '../DataInventory';
-import { NavLink } from 'react-router-dom';
-import './index.css';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-
+import React from "react";
+import find from "lodash/find";
+import { Icon } from "react-materialize";
+import { Link } from "react-router-dom";
+import { getProducts } from "../DataInventory";
+import { NavLink } from "react-router-dom";
+import "./index.css";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
 
 export default class ProItems extends React.Component {
-
-
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   handleChange(event) {
@@ -47,123 +43,132 @@ export default class ProItems extends React.Component {
     //console.log(this.state.value);
     //event.preventDefault();
   }
+  checkout() {
+    console.log("you can do this!");
+  }
+
   remove(ID) {
+    var cart = localStorage.getItem("item");
 
-    var cart = localStorage.getItem('item');
-
-    var array_value = JSON.parse(localStorage.getItem('item'));
-
+    var array_value = JSON.parse(localStorage.getItem("item"));
 
     var cartObj = [];
     for (var i = 0; i < array_value.length; i++) {
-
       if (ID != array_value[i].ID) {
-
         cartObj.push(array_value[i]);
-
       }
-
     }
 
     localStorage.setItem("item", JSON.stringify(cartObj));
-
   }
 
-  add(ID, stock) {
-
+  update(ID, stock) {
     // console.log(ID);
     // console.log(stock);
 
     if (stock < this.state.value) {
-
       alert("Please Enter less ammount of items");
-
     } else {
-
-
-
       alert(this.state.value + "  Items add to the checkout successfully ! ");
     }
 
-
-
     // Get access_token from localstorage
-    var access_token = localStorage.getItem('access_token');
+    var access_token = localStorage.getItem("access_token");
 
     //console.log(this.inputNode.value);
 
     let axiosConfig = {
       headers: {
-
         "Access-Control-Allow-Origin": "*",
-        "Accept": "*/*",
+        Accept: "*/*",
 
         //"Authorization": "Bearer " + access_token
 
-        "Authorization": "Bearer 12ee492a-e7d1-3fa5-937e-2970b5225adc"
-
+        Authorization: "Bearer 12ee492a-e7d1-3fa5-937e-2970b5225adc"
       }
     };
 
-    // by this we send put request through the inventory_API for add Items for the store 
+    // by this we send put request through the inventory_API for add Items for the store
 
-    return axios.put("https://localhost:8243/orderapi/1.0.0/order/" + ID, { "stock": eval(stock) - eval(this.state.value) }, axiosConfig) //FRONTEND_URL
-      .then(function (res) {
+    return axios
+      .put(
+        "https://localhost:8243/orderapi/1.0.0/order/" + ID,
+        { stock: eval(stock) - eval(this.state.value) },
+        axiosConfig
+      ) //FRONTEND_URL
+      .then(function(res) {
         console.log("RESPONSE RECEIVED: ", res);
         console.log("RESPONSE data: ", res.data);
         let products = res.data;
-        return (products);
-      }).catch((err) => {
+        return products;
+      })
+      .catch(err => {
         console.log("AXIOS ERROR: ", err);
-
       });
-
   }
 
   render() {
-
-    var cart_items = JSON.parse(localStorage.getItem('item'));
+    var cart_items = JSON.parse(localStorage.getItem("item"));
 
     if (cart_items == null) {
-
-      return (<div className="Empty_headder"><h4>Cart is Empty</h4></div>);
+      return (
+        <div className="Empty_headder">
+          <h4>Cart is Empty</h4>
+        </div>
+      );
     }
 
     return (
+      <div>
+        {cart_items.map(product => {
+          return (
+            <div className="product-items">
+              <div className="product">
+                <h1 id="product-name">{product.name}</h1>
+                <h4 id="product-description">Product ID :{product.ID}</h4>
+                <h5 id="product-price">${product.price}</h5>
 
+                <div className="item-form">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Quentity:
+                      <input
+                        type="Number"
+                        value="1"
+                        ref={this.state.value}
+                        onChange={this.handleChange}
+                      />
+                    </label>
 
-      <div>{cart_items.map((product) => {
-        return (
-
-          <div className="product-items">
-
-            <div className="product">
-              <h1 id="product-name">{product.name}</h1>
-              <h4 id="product-description">Product ID :{product.ID}</h4>
-              <h5 id="product-price">${product.price}</h5>
-
-              <div className="item-form">
-                <form onSubmit={this.handleSubmit}>
-                  <label>
-                    Quentity:
-          <input type="Number" ref={this.state.value} onChange={this.handleChange} />
-
-                  </label>
-
-                  <input type="submit" value="Add" onClick={() => this.add(product.ID, product.stock)} />
-
-                  <input type="submit" value="Remove" onClick={() => this.remove(product.ID)} />
-
-                </form>
+                    <Button
+                      onClick={() => this.update(product.ID, product.stock)}
+                      variant="contained"
+                      color="primary"
+                    >
+                      Update
+                    </Button>
+                    <Button
+                      onClick={() => this.remove(product.ID)}
+                      variant="contained"
+                      color="secondary"
+                    >
+                      Remove
+                    </Button>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
-
-
-
-
-        )
-      })}
+          );
+        })}
+        <span styles={{ float: "right" }}>
+          <Button
+            onClick={() => this.checkout()}
+            variant="contained"
+            color="secondary"
+          >
+            CheckOut
+          </Button>
+        </span>
       </div>
     );
   }
